6ab87144c68318b389d35f0a60e4247d71a950c5
analog-nico <nicolai.kamenzky@testrails.org>
2014-09-16 16:07:56 -0400

    Improved loading

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,28 +1,30 @@
 // Paste this code into e.g. http://ted.mielczarek.org/code/mozilla/bookmarklet.html
 
-var head = document.getElementsByTagName('head')[0];
-
-function loadJQuery() {
+function loadScript(url) {
     var s = document.createElement('script');
-    s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
-    head.appendChild(s);
+    s.setAttribute('src', url);
+    document.getElementsByTagName('head')[0].appendChild(s);
 }
 
-function loadSpectrum() {
+function loadCss(url) {
     var r = document.createElement('link');
     r.setAttribute('rel', 'stylesheet');
     r.setAttribute('type', 'text/css');
-    r.setAttribute('href', 'https://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.css');
-    head.appendChild(r);
-    var s = document.createElement('script');
-    s.setAttribute('src','https://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.js');
-    head.appendChild(s);
+    r.setAttribute('href', url);
+    document.getElementsByTagName('head')[0].appendChild(r);
+}
+
+function loadJQuery() {
+    loadScript('//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+}
+
+function loadSpectrum() {
+    loadCss('//cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.css');
+    loadScript('//cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.min.js');
 }
 
 function loadColorizr() {
-    var s = document.createElement('script');
-    s.setAttribute('src','https://raw.githubusercontent.com/analog-nico/colorizr/master/src/colorizr.js');
-    head.appendChild(s);
+    loadScript('//raw.githubusercontent.com/analog-nico/colorizr/master/src/colorizr.js');
 }
 
 if (window.jQuery === undefined) {
@@ -32,6 +34,6 @@ if (window.jQuery === undefined) {
 } else if (window.jQuery.fn.spectrum === undefined) {
     loadSpectrum();
     loadColorizr();
-} else if (window.jQuery('.colorizr').length === 0) {
+} else if (window.jQuery('#colorizr').length === 0) {
     loadColorizr();
 }
