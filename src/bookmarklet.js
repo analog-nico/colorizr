4c21541051c4d75c48a323e4bd7799c82b82e797
analog-nico <nicolai.kamenzky@testrails.org>
2014-09-16 14:59:25 -0400

    Improved reliability

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,37 +1,37 @@
-(function() {
+// Paste this code into e.g. http://ted.mielczarek.org/code/mozilla/bookmarklet.html
 
-    function loadJQuery() {
-        var s = document.createElement('script');
-        s.setAttribute('src','http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
-        document.getElementsByTagName('head')[0].appendChild(s);
-    }
+var head = document.getElementsByTagName('head')[0];
 
-    function loadSpectrum() {
-        var r = document.createElement('link');
-        r.setAttribute('rel', 'stylesheet');
-        r.setAttribute('type', 'text/css');
-        r.setAttribute('href', 'http://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.css');
-        document.getElementsByTagName('head')[0].appendChild(r);
-        var s = document.createElement('script');
-        s.setAttribute('src','http://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.js');
-        document.getElementsByTagName('head')[0].appendChild(s);
-    }
+function loadJQuery() {
+    var s = document.createElement('script');
+    s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+    head.appendChild(s);
+}
 
-    function loadColorizr() {
-        var s = document.createElement('script');
-        s.setAttribute('src','http://raw.githubusercontent.com/analog-nico/colorizr/master/src/index.js');
-        document.getElementsByTagName('head')[0].appendChild(s);
-    }
+function loadSpectrum() {
+    var r = document.createElement('link');
+    r.setAttribute('rel', 'stylesheet');
+    r.setAttribute('type', 'text/css');
+    r.setAttribute('href', 'https://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.css');
+    head.appendChild(r);
+    var s = document.createElement('script');
+    s.setAttribute('src','https://raw.githubusercontent.com/bgrins/spectrum/master/spectrum.js');
+    head.appendChild(s);
+}
 
-    if (window.jQuery === undefined) {
-        loadJQuery();
-        loadSpectrum();
-        loadColorizr();
-    } else if (window.jQuery.fn.spectrum === undefined) {
-        loadSpectrum();
-        loadColorizr();
-    } else if (window.jQuery('.colorizr').length === 0) {
-        loadColorizr();
-    }
+function loadColorizr() {
+    var s = document.createElement('script');
+    s.setAttribute('src','https://raw.githubusercontent.com/analog-nico/colorizr/master/src/index.js');
+    head.appendChild(s);
+}
 
-}());
+if (window.jQuery === undefined) {
+    loadJQuery();
+    loadSpectrum();
+    loadColorizr();
+} else if (window.jQuery.fn.spectrum === undefined) {
+    loadSpectrum();
+    loadColorizr();
+} else if (window.jQuery('.colorizr').length === 0) {
+    loadColorizr();
+}
