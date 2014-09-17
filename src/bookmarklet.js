ed005fc7e69f4be9abe25e9b3c0e6c0df51584ee
analog-nico <nicolai.kamenzky@testrails.org>
2014-09-17 14:06:59 -0400

    Loading just the bundled build

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,39 +1,5 @@
 // Paste this code into e.g. http://ted.mielczarek.org/code/mozilla/bookmarklet.html
 
-function loadScript(url) {
-    var s = document.createElement('script');
-    s.setAttribute('src', url);
-    document.getElementsByTagName('head')[0].appendChild(s);
-}
-
-function loadCss(url) {
-    var r = document.createElement('link');
-    r.setAttribute('rel', 'stylesheet');
-    r.setAttribute('type', 'text/css');
-    r.setAttribute('href', url);
-    document.getElementsByTagName('head')[0].appendChild(r);
-}
-
-function loadJQuery() {
-    loadScript('//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
-}
-
-function loadSpectrum() {
-    loadCss('//cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.css');
-    loadScript('//cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.min.js');
-}
-
-function loadColorizr() {
-    loadScript('//raw.githubusercontent.com/analog-nico/colorizr/master/dist/colorizr.js');
-}
-
-if (window.jQuery === undefined) {
-    loadJQuery();
-    loadSpectrum();
-    loadColorizr();
-} else if (window.jQuery.fn.spectrum === undefined) {
-    loadSpectrum();
-    loadColorizr();
-} else if (window.jQuery('#colorizr').length === 0) {
-    loadColorizr();
-}
+var s = document.createElement('script');
+s.setAttribute('src', url);
+document.getElementsByTagName('head')[0].appendChild('//raw.githubusercontent.com/analog-nico/colorizr/master/dist/colorizr.bundled.min.js');
