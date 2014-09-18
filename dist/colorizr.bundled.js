743f2a30b4627cd29fa8cc46f296adcf72ff9bdb
analog-nico <nicolai.kamenzky@testrails.org>
2014-09-17 23:01:33 -0400

    "For better recollection"

--- dist/colorizr.bundled.js
+++ dist/colorizr.bundled.js
@@ -12654,7 +12654,7 @@ return jQuery;
 
     var panel = $('<div id="colorizr" class="clrz-panel clrz-reset">');
     panel.append($('<div class="clrz-header clrz-reset">' +
-        '<div class="clrz-title-title clrz-reset">Title to remember</div>' +
+        '<div class="clrz-title-title clrz-reset">For better recollection</div>' +
         '<div class="clrz-title-target clrz-reset">HTML Element Selector</div>' +
         '<div class="clrz-title-manipulation clrz-reset">Manipulation of</div>' +
         '<div class="clrz-title-name clrz-reset">Property or Attribute Name</div>' +
