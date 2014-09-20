0b08e83a8a1d643d92d7b8856e6b5227ce70846e
analog-nico <nicolai.kamenzky@testrails.org>
2014-09-20 19:55:14 -0400

    Bookmarklet points to Bitly

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,5 +1,5 @@
 // Paste this code into e.g. http://ted.mielczarek.org/code/mozilla/bookmarklet.html
 
 var s = document.createElement('script');
-s.setAttribute('src', 'https://analog-nico.github.io/colorizr/js/colorizr.bundled.min.js');
+s.setAttribute('src', 'https://bit.ly/colorizr-js');
 document.getElementsByTagName('head')[0].appendChild(s);
